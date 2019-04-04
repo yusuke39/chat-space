bd76782fe7505566b5aeafbc3c8de74237886d2f
yusuke39 <hiranoyusuke39@gmail.com>
2019-04-04 16:13:50 +0900

    edit_groups

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+devise_for :users
 root 'messages#index'
-
+resources :users, only: [:edit, :update]
+resources :groups, only:[:new, :create, :update, :edit] do
+ end
 end
