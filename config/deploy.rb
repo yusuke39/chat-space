a37db4889510cceaaafbeeb6f47c97eee2b2487b
yusuke39 <hiranoyusuke39@gmail.com>
2019-04-22 17:35:35 +0900

    edit_deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -55,6 +55,8 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
+set :linked_files, %w{ config/secrets.yml }
+
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
@@ -80,5 +82,3 @@ set :default_env, {
   AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
   AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
 }
-
-set :linked_files, %w{ config/secrets.yml }
